6e61e31a4e415b71f6a67b541322eaff9ce3917f
Marwa Gaser <marwagaser@gmail.com>
2020-03-03 17:50:24 +0200

    fix: fixed a minor bug in the rectangle's draw function

--- rectangle.js
+++ rectangle.js
@@ -1,27 +1,28 @@
 class Rectangle {
-    constructor(minX, minY, maxX, maxY) {
-        if (minX >= maxX || minY >= maxY) {
-            throw `invalid boundaries ${minX}, ${minY}, ${maxX}, ${maxY}`
-        }
-        this.minX = minX;
-        this.maxX = maxX;
-        this.maxY = maxY;
-        this.minY = minY;
-
+  constructor(minX, minY, maxX, maxY) {
+    if (minX >= maxX || minY >= maxY) {
+      throw `invalid boundaries ${minX}, ${minY}, ${maxX}, ${maxY}`;
     }
-    draw(stage) {
-        this.shape = new createjs.Shape();
-        this.shape.graphics.beginStroke("#000000");
-        this.shape.graphics.beginFill("#ffffff");
-        this.shape.graphics.drawRect(this.minX, this.minY, this.maxX - this.minX, this.maxY -  this.minY);
-        this.shape.graphics.endFill();
-        stage.addChild(this.shape);
-        stage.update();
-    }
+    this.minX = minX;
+    this.maxX = maxX;
+    this.maxY = maxY;
+    this.minY = minY;
+  }
+  draw(stage) {
+    this.shape = new createjs.Shape();
+    this.shape.graphics.beginStroke('#000000');
+    this.shape.graphics.drawRect(
+      this.minX,
+      this.minY,
+      this.maxX - this.minX,
+      this.maxY - this.minY
+    );
+    this.shape.graphics.endFill();
+    stage.addChild(this.shape);
+    stage.update();
+  }
 
-    isLeftOf(rectangle) {
-        return this.maxY == rectangle.maxY &&
-            this.minX >= rectangle.maxX;
-    }
-
+  isLeftOf(rectangle) {
+    return this.maxY == rectangle.maxY && this.minX >= rectangle.maxX;
+  }
 }
